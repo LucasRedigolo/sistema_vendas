92a589881d7e5b244c078cb7f94c48f2d56b12be
Edilson <edilson_js@yahoo.com.br>
2017-10-19 21:18:06 -0200

    realizado menu de opções

--- Program.cs
+++ Program.cs
@@ -6,7 +6,61 @@ namespace sistema_vendas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string opcao = "";
+
+            do
+            {
+                System.Console.WriteLine("Digite a opção desejada:");
+                System.Console.WriteLine("1 - Cadastrar Cliente");
+                System.Console.WriteLine("2 - Cadastrar Prouduto");
+                System.Console.WriteLine("3 - Realizar Venda");
+                System.Console.WriteLine("4 - Extrato Cliente");
+                System.Console.WriteLine("9 - Sair");
+
+                opcao = Console.ReadLine();
+                // Recebe dado dos clientes
+
+                switch (opcao)
+                {
+                    case "1":
+                        CadastrarCliente();
+                    break;
+
+                    case "2":
+                        CadastrarProduto();
+                    break;
+
+                    case "3":
+                        RealizarVenda();
+                    break;
+
+                    case "4":
+                        ExtratoCliente();
+                    break;        
+                }
+
+            }
+            while (opcao != "9");
         }
-    }
+
+            // Cadastrar novo cliente
+            static void CadastrarCliente(){
+
+            }
+
+            // Cadastrar novo Produto
+            static void CadastrarProduto(){
+
+            }
+
+            // Realizar Venda
+            static void RealizarVenda(){
+
+            }
+
+            // Extrato do Cliente
+            static void ExtratoCliente(){
+
+            }
+    }   
 }
