30acc3273c4c00eee0f43b7802904e83d8edf834
Edilson <edilson_js@yahoo.com.br>
2017-10-19 22:00:04 -0200

    fim da aula

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿using System;
-
+﻿ using System;
+/*
 namespace sistema_vendas
 {
     class Program
@@ -44,9 +44,37 @@ namespace sistema_vendas
         }
 
             // Cadastrar novo cliente
-            static void CadastrarCliente(){
+            static void CadastrarCliente()
+            {
+                System.Console.WriteLine("Digite seu nome:");
+                Console.ReadLine();
+
+                System.Console.WriteLine("Digite Seu Email");
+                Console.ReadLine();
+
+                System.Console.WriteLine("Digite a Opção correta");
+                System.Console.WriteLine("1 - Pessoa Física");
+                System.Console.WriteLine("2 - Pessoa Jurídica");
+                 = Console.ReadLine();
+
+                switch ()
+                {   
+                    case "1":
+                    System.Console.WriteLine("Digite seu CPF:");
+                    Console.ReadLine();
+                    break;
+
+                    case "2":
+                    System.Console.WriteLine("Digite seu CNPJ");
+                    Console.ReadLine();
+                    break;
+                    
+                }
+
 
             }
+
+           
 
             // Cadastrar novo Produto
             static void CadastrarProduto(){
@@ -62,5 +90,78 @@ namespace sistema_vendas
             static void ExtratoCliente(){
 
             }
-    }   
+
+        static bool ValidaCPF (string cpf){
+
+                
+            }
+            static void checagemcpf(string[] args)
+        {
+            Console.WriteLine("Digite seu CPF");
+            string cpf = Console.ReadLine();
+
+            if(cpf.Length != 11)
+            {
+                Console.WriteLine("CPF Inválido");
+            }
+            else
+            {
+                int[] multiplicador1 = new int[]{10,9,8,7,6,5,4,3,2};
+                int[] multiplicador2 = new int[]{11,10,9,8,7,6,5,4,3,2};
+                string cpf9, cpf10, cpfult2, digito, digito1, digito2;
+                int soma1=0, resto1=0, soma2=0, resto2=0;
+                cpf9 = cpf.Substring(0,9);
+                cpfult2 = cpf.Substring(9,2);
+
+                for(int i = 0; i < 9; i++)
+                {
+                    soma1 += Convert.ToInt16(cpf9[i].ToString()) * multiplicador1[i];                     
+                }
+
+                resto1 = soma1%11;
+
+                if(resto1 < 2)
+                {
+                    resto1 = 0;
+                }
+                else
+                {
+                    resto1 = 11 - resto1;
+                }
+                
+                digito1 = resto1.ToString();
+                cpf10 = cpf9 + digito1;
+                              
+                
+                for(int i=0;i<10;i++)
+                {
+                    soma2 += Convert.ToInt32(cpf10[i].ToString()) * multiplicador2[i]; 
+                }
+                
+                resto2 = soma2%11;
+
+                if(resto2 < 2)
+                {
+                    resto2 = 0;
+                }
+                else
+                {
+                    resto2 = 11 - resto2;
+                }
+
+                digito2 = resto2.ToString();
+                digito = digito1 + digito2;
+
+                if(digito==cpfult2.ToString())
+                {
+                    Console.WriteLine("CPF Válido");
+                }
+                 else
+                {
+                     Console.WriteLine("CPF Inválido");
+                }
+            }
+
+        }    
 }
+*/
